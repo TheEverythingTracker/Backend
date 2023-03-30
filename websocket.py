74b0f4b5a89bdf0261c7a902267f6beb8d9a10ca
Hypnos8 <justsleeping22@gmail.com>
2023-03-30 19:28:20 +0200

    Fixed issue with websocket async handler

--- websocket.py
+++ websocket.py
@@ -1,16 +1,20 @@
 import asyncio
+import multiprocessing
 import queue
 
 import websockets
 
-bounding_box_queue = queue.Queue(20)
-
+bounding_box_queue = multiprocessing.Queue(20)
 
 async def __consumer_handler(websocket):
     print("producer_handler started")
     while True:
         message = bounding_box_queue.get()  # todo: discard frames if queue full?
         await websocket.send(message)
+        # If you run a loop that contains only synchronous operations and a send() call,
+        # you must yield control explicitly with asyncio.sleep():
+        # https://websockets.readthedocs.io/en/stable/faq/asyncio.html
+        await asyncio.sleep(0)
 
 
 async def run_websocket_server():
