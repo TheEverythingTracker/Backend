8beef4454a8c7073c85f3d14f880f24e4aca68ba
Hypnos8 <justsleeping22@gmail.com>
2023-03-30 17:01:09 +0200

    Added websocket test

---
+++ websocket_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+        while True:
+             message = await websocket.recv()
+             print(message)
+
+asyncio.run(hello())
