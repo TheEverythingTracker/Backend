a4d41cd0a3993f31a0b9341fcd7489f98b456d3d
nico <23122368+ninolus@users.noreply.github.com>
2023-04-02 12:04:16 +0200

    implement WorkerProcess class for managing process-related resources

--- worker/worker.py
+++ worker/worker.py
@@ -1,12 +1,24 @@
+import multiprocessing
+
 from worker import tracking
 from errors import TrackingError
 
 
-def do_work(img, bounding_box, conn2, queue):
-    tracker = tracking.Tracker(img, bounding_box, conn2, queue)
+class WorkerProcess:
+    def __init__(self, img, bounding_box: tuple, queue: multiprocessing.Queue):
+        self.to_worker, self.to_master = multiprocessing.Pipe()
+        self.has_quit = multiprocessing.Event()
+        self.process = multiprocessing.Process(target=do_work,
+                                               args=(img, bounding_box, self.to_worker, queue, self.has_quit))
+
+
+def do_work(img, bounding_box: tuple, to_worker: multiprocessing.Pipe, queue: multiprocessing.Queue,
+            has_quit: multiprocessing.Event):
+    tracker = tracking.Tracker(img, bounding_box, to_worker, queue)
     try:
         tracker.run_tracking_loop()
     except (TrackingError, IOError) as e:
         print(e)
-        return
-    # TODO: raise process deletion from process array
+        has_quit.set()
+        exit(1)
+# TODO: raise process deletion from process array
